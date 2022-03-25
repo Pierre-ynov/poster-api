d96b81c0b7b730b7b4e77943ff8be60616689a64
Pierre VILLIERS <pierre.villiers@ynov.com>
2022-03-25 10:43:02 +0100

    Initialisation du projet spring

---
+++ src/main/java/com/webservices/posterapi/PosterApiApplication.java
@@ -0,0 +1,13 @@
+package com.webservices.posterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservices/posterapi/PosterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.posterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
